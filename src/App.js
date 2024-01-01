042aef4f8699d11990a63cf867abe8dc0bbf182b
Tung Nguyen <nvntung@gmail.com>
2024-01-01 21:26:27 +0000

    Test reactstrap
    npm install --save reactstrap bootstrap

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import AppNavBar from "./shared/NavBar";
+import BootstrapGridTest from "./shared/MainLayout";
 
 function App() {
   return (
@@ -18,6 +20,8 @@ function App() {
           Learn React
         </a>
       </header>
+      <AppNavBar/>
+      <BootstrapGridTest/>
     </div>
   );
 }
---
+++ src/shared/MainLayout.js
@@ -0,0 +1,19 @@
+import {Button, Container} from "reactstrap";
+
+function BootstrapGridTest() {
+    return (
+        <Container>
+            <h1>Fluid Grid!</h1>
+            <p>
+                This is a modified fluid which
+                stretches the whole horizontal space.
+            </p>
+            <Button color="primary">
+                Submit
+            </Button>
+            <Button color="danger">Danger!</Button>
+        </Container>
+    );
+}
+
+export  default BootstrapGridTest;
