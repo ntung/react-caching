58aab8f90ba78c122b9ca7ba545ab64a4a5be7d5
Tung Nguyen <nvntung@gmail.com>
2024-01-02 16:47:37 +0000

    Demonstrate how React renders components

--- src/App.js
+++ src/App.js
@@ -1,27 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
-import AppNavBar from "./shared/NavBar";
-import BootstrapGridTest from "./shared/MainLayout";
+import SumComp from "./shared/SumComp";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <AppNavBar/>
-      <BootstrapGridTest/>
+      <SumComp/>
     </div>
   );
 }
---
+++ src/shared/SumComp.js
@@ -0,0 +1,46 @@
+import React, {useState, useMemo} from 'react';
+
+function SumComp() {
+    console.log("Rendering SumComp component...");
+    const COLOURS = ['aqua', 'blue', 'fuchsia', 'gray', 'green',
+        'lime', 'maroon', 'navy', 'olive', 'orange', 'purple', 'red',
+        'silver', 'teal', 'yellow'];
+    const cId = randomColor(COLOURS.length);
+    const [colorName, setColorName] = useState(COLOURS[cId]);
+    const [txt, setTxt] = useState("Some text");
+    const [a, setA] = useState(0);
+    const [b, setB] = useState(0);
+    // const sum = useMemo(() => {
+    //     console.log("Computing sum...");
+    //     return a + b;
+    // }, [a, b]);
+    const sum = expensiveComputing(a, b);
+    return (
+        <div>
+            <p>Text: <span style={{"color": colorName}}>{txt}</span></p>
+            <p>a: {a}</p>
+            <p>b: {b}</p>
+            <p>sum: {sum}</p>
+            <button onClick={() => {
+                const text = Math.random().toString(36).slice(2, 7);
+                setTxt(text);
+                const colorIndex = randomColor(COLOURS.length);
+                const colorName = COLOURS[colorIndex];
+                setColorName(colorName);
+            }}>Click here to change text and color</button>
+            <button onClick={() => setA(a + 1)}>Increment a</button>
+            <button onClick={() => setB(b + 1)}>Increment b</button>
+        </div>
+    );
+}
+
+function randomColor(max) {
+    console.log("Calling random color index...")
+    return Math.floor(Math.random()*max);
+}
+function expensiveComputing(x, y) {
+    console.log("Calling expensive computing...");
+    return x + y;
+}
+
+export default SumComp;
